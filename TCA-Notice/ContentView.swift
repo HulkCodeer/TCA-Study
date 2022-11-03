3c931d2171bf7a8a1582309586be72b8a1c79f36
hyunjin.park <unicon85@gmail.com>
2022-11-03 21:26:40 +0900

    :construction: 프로젝트 셋팅

---
+++ TCA-Notice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCA-Notice
+//
+//  Created by 박현진 on 2022/11/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCA-Notice/TCA_NoticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_NoticeApp.swift
+//  TCA-Notice
+//
+//  Created by 박현진 on 2022/11/03.
+//
+
+import SwiftUI
+
+@main
+struct TCA_NoticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
